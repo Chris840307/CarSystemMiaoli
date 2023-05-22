730a636b3aa7db8b79b6314a9d2af566e7ec8312
Chris_lu <66943605+Chris840307@users.noreply.github.com>
2023-05-22 22:57:59 +0800

    新增照片上傳

---
+++ php/postCaseImage.php
@@ -0,0 +1,67 @@
+<?php
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '2u6u/ru8';
+$dbname = 'car';
+
+$str = file_get_contents('php://input');
+$str_json = json_decode($str);
+$SN = $str_json->{'SN'};
+$No = $str_json->{'No'};
+
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
+
+mysqli_query($conn, 'SET NAMES utf8');
+mysqli_select_db($conn, $dbname);
+
+//jpg傳base64
+$sql = "SELECT * FROM `violation` WHERE `RowNo` = '$SN'";
+$result = mysqli_query($conn, $sql) or die('MySQL select error' . mysqli_error($conn));
+if ($result->num_rows > 0) {
+    while ($record = mysqli_fetch_array($result)) {
+        $path = $record['PhotoURL'];
+        $data = file_get_contents($path);
+        $base64 = base64_encode($data);
+    }
+}
+
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://223.200.44.147/' . $No,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => array('Data' => $base64),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+
+//寫入DB留紀錄
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
+
+mysqli_query($conn, 'SET NAMES utf8');
+mysqli_select_db($conn, $dbname);
+
+$obj = json_decode($response);
+$Message = $obj->Message;
+
+$sql = "INSERT INTO `case_image`(`SN`, `No`, `Message`) VALUES ('$SN','$No','$Message')";
+$result = mysqli_query($conn, $sql) or die('MySQL select error' . mysqli_error($conn));
+
+
+echo $response;
+// echo $str;
